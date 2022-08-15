05c470d759d8b5e7df06b2cea3ca840a97b63f05
B <77552838+Mrballer59@users.noreply.github.com>
2022-08-15 21:48:22 +0200

    header added + css

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Header.css";
+import logo from "../../assets/logosport.png";
+
+const Header = () => {
+  return (
+    <header>
+      <img className="logo" alt="logo of company" src={logo} />
+      <a href="#">Accueil</a>
+      <a href="#">Profil</a>
+      <a href="#">Réglage</a>
+      <a href="#">Communauté</a>
+    </header>
+  );
+};
+
+export default Header;
