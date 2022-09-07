0a0fd0b6fcd7f5acd4b570a902f71d57b8ea0b57
B <77552838+Mrballer59@users.noreply.github.com>
2022-09-07 21:34:37 +0200

    lineChart days of the week + Css and responsive

--- src/components/LineChart/LineChart.jsx
+++ src/components/LineChart/LineChart.jsx
@@ -12,6 +12,27 @@ import {
 
 function SessionsChart(data) {
   console.log(data);
+  const daysWeek = {
+    1: "L",
+    2: "M",
+    3: "M",
+    4: "J",
+    5: "V",
+    6: "S",
+    7: "D",
+  };
+  const listedDays = (days) => daysWeek[days];
+
+  const CustomToolTip = ({ active, payload }) => {
+    if (active && payload && payload.length) {
+      return (
+        <div className="customToolTip">
+          <p className="lable">{`${payload[0].value}min`}</p>
+        </div>
+      );
+    }
+    return null;
+  };
   return (
     <div className="averageSessions">
       <h2>{data.title}</h2>
@@ -26,10 +47,10 @@ function SessionsChart(data) {
             tick={{ fill: "#ffffff" }}
             tickLine={{ stroke: "" }}
             axisLine={{ stroke: "" }}
+            tickFormatter={listedDays}
           />
           <YAxis hide={true} />
-          <Tooltip />
-          <Legend />
+          <Tooltip content={CustomToolTip} cursor={false} />
           <Line
             type="monotone"
             dataKey={data.dataLength}
