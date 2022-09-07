62ce4a0a4061b8582dfdec0e452f062553497b07
B <77552838+Mrballer59@users.noreply.github.com>
2022-09-07 21:32:32 +0200

    fixed barchart css + customTip

--- src/components/BarChart/BarChart.jsx
+++ src/components/BarChart/BarChart.jsx
@@ -10,45 +10,79 @@ import {
   Legend,
   ResponsiveContainer,
 } from "recharts";
-function ActivityChartBar(data) {
-  console.log(data);
+
+function ActivityChartBar(props) {
+  const formatterDate = (item) => new Date(item).getDate();
+  const CustomToolTip = ({ active, payload }) => {
+    if (active && payload && payload.length) {
+      return (
+        <div className="custom-tooltip">
+          <span className="lable">{`${payload[0].value}kg`}</span>
+          <span className="lable">{`${payload[1].value}Kcal`}</span>
+        </div>
+      );
+    }
+    return null;
+  };
+  // console.log(props);
   return (
     <div className="activityChartBar">
       <div className="activityChartHeader">
-        <h2>{data.title}</h2>
+        <h2>{props.title}</h2>
         <ul>
-          <li className="blackCircle">{data.legendDataKg}</li>
-          <li className="redCircle">{data.legendData2}</li>
+          <li className="blackCircle">{props.legendDataKg}</li>
+          <li className="redCircle">{props.legendData2}</li>
         </ul>
       </div>
       <ResponsiveContainer width="100%" height="100%">
         <BarChart
-          data={data.data}
+          data={props.data}
           margin={{
             top: 20,
             left: 5,
           }}
+          barGap={7}
         >
-          <CartesianGrid vertical={false} />
-          <XAxis dataKey={data.xDataKey} tickLine={false} />
+          <CartesianGrid vertical={false} strokeDasharray="3 3" />
+          <XAxis
+            dataKey={props.xDataKey}
+            tickLine={false}
+            tickFormatter={formatterDate}
+          />
           <YAxis
-            dataKey={data.dataKg}
+            dataKey={props.dataKg}
+            yAxisId="kilogram"
             orientation="right"
             axisLine={false}
             tickLine={false}
+            domain={["dataMin-2", "dataMax+2"]}
+            tickCount={5}
           />
-          <YAxis dataKey={data.dataCal} hide={true} />
-          <Tooltip />
-          <Legend />
+          <YAxis
+            dataKey={props.dataCal}
+            axisLine={false}
+            tickLine={false}
+            hide={true}
+            yAxisId="calories"
+            orientation="right"
+            domain={["dataMin-50", "dataMax+50"]}
+          />
+          <Tooltip
+            content={<CustomToolTip />}
+            // cursor={{ fill: "#C4C4C4", width: 65, transform: "translate(28)" }}
+          />
+          {/* <Legend /> */}
           <Bar
-            dataKey={data.dataKg}
+            dataKey={props.dataKg}
             fill="#282D30"
             barSize={10}
+            yAxisId="kilogram"
             radius={[5, 5, 0, 0]}
           />
           <Bar
-            dataKey={data.dataCal}
+            dataKey={props.dataCal}
             fill="#E60000"
+            yAxisId="calories"
             barSize={10}
             radius={[5, 5, 0, 0]}
           />
